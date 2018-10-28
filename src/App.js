bf4975a43fe307de5da4804b90c39e8a626339c5
Milly Rodriguez <millz118@gmail.com>
2018-10-28 14:52:55 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import List from "./List";
+import axios from "axios";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            people: []
+        };
+
+        this.getPeople = this.getPeople.bind(this);
+    }
+
+    getPeople() {
+        return axios
+            .get("http://swapi.co/api/people", {
+                headers: {
+                    "Access-Control-Allow-Origin": "*"
+                }
+            })
+            .then(response => {
+                console.log(response.data.results);
+                this.setState({ people: response.data.results });
+            });
+    }
+
+    componentDidMount() {
+        this.getPeople();
+    }
+
+    render() {
+        // const { people } = this.state;
+        return <div className="App">{/* <List people={people} /> */}</div>;
+    }
 }
 
 export default App;
